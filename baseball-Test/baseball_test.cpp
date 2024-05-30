bab5a2658159ac04ab79477cec168ba908f1c472
kwonluck <software_k@naver.com>
2024-05-30 15:24:17 +0900

    input 관련 Execption 처리 구현

--- baseball-Test/baseball_test.cpp
+++ baseball-Test/baseball_test.cpp
@@ -1,6 +1,27 @@
 #include "pch.h"
 #include "../BaseballGame/baseball.cpp"
+#include <stdexcept>
+using namespace std;
 
-TEST(BaseballGame, gameTest) {
-  EXPECT_EQ(1, 1);
+class BaseballFixture : public testing::Test {
+public :
+	Baseball game;
+	void assertIllegalArgument(string guessNumber) {
+		try
+		{
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e)
+		{
+			//pass
+		}
+	}
+};
+
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInputInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
+	assertIllegalArgument("121");
 }
