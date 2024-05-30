0ffbcc22f665f3c500b3da418e371e1a0dd033ac
kwonluck <software_k@naver.com>
2024-05-30 15:39:12 +0900

    Reapply "input 관련 Execption 처리 구현"
    This reverts commit 63da9ba8a160c7f456cb67ac52e8162e7a409789.

--- BaseballGame/baseball.cpp
+++ BaseballGame/baseball.cpp
@@ -1,5 +1,36 @@
+#include <string>
+#include <stdexcept>
+using namespace std;
+
 
 class Baseball {
 public:
+	void guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+	}
 
+private:
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
+		if (guessNumber.size() != 3)
+		{
+			throw length_error("Must be three letters.");
+		}
+
+		for (char ch : guessNumber)
+		{
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("must be number");
+		}
+
+		if (isDuplicatedNumber(guessNumber))
+			throw invalid_argument("must not have same number");
+	}
+
+	bool isDuplicatedNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[1] == guessNumber[2]
+			|| guessNumber[2] == guessNumber[0];
+	}
 };
